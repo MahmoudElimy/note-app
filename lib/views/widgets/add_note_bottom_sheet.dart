9806c22bc538265eeee973b7684335941fa38ce6
MahmoudElimy <mahmoudelimy27@gmail.com>
2025-04-17 15:46:56 +0200

    finish note bottom sheet

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:noote_app/views/widgets/custom_button.dart';
 import 'package:noote_app/views/widgets/custom_text_field.dart';
 
 class AddNoteModalBottomSheet extends StatelessWidget {
@@ -8,13 +9,18 @@ class AddNoteModalBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(height: 32),
-          CustomFormTextField(hintText: 'title'),
-          SizedBox(height: 16),
-          CustomFormTextField(hintText: 'content', maxLines: 5),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 32),
+            CustomFormTextField(hintText: 'title'),
+            SizedBox(height: 16),
+            CustomFormTextField(hintText: 'content', maxLines: 5),
+            SizedBox(height: 32),
+            CustomButton(text: 'Add'),
+            SizedBox(height: 16),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/views/widgets/custom_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:noote_app/constats.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+
+  CustomButton({super.key, this.onTap, required this.text});
+  VoidCallback? onTap;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: kPrimaryColor,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        width: MediaQuery.of(context).size.width,
+        height: 55,
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
