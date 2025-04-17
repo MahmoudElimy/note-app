36f70c3c8b41a4bade45e3f4625685776864dde8
MahmoudElimy <mahmoudelimy27@gmail.com>
2025-04-17 12:48:32 +0200

    create custom note item

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ class NoteApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark,fontFamily: 'Poppins'),
       home:const NotesView(),
     );
   }
---
+++ lib/views/widgets/custom_icon.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CustomIcon extends StatelessWidget {
+  const CustomIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 45,
+      width: 45,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.white.withOpacity(0.05),
+      ),
+      child: Center(child: Icon(Icons.search, color: Colors.white, size: 28)),
+    );
+  }
+}
